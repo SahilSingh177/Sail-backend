4090d46f42b0292fe576477229dc39cd020bbf6a
chakri68 <chakridevireddy69@gmail.com>
2024-03-03 15:45:21 +0530

    removed redundant app files

--- app.py
+++ app.py
@@ -1,120 +1,99 @@
 import streamlit as st
-from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-# from langchain.vectorstores import FAISS
-from langchain_community.vectorstores import FAISS
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
-# from langchain.chat_models import ChatOpenAI
-# from langchain.llms import HuggingFaceHub
-from langchain_community.llms import HuggingFaceHub
-from htmlTemplates import bot_template, user_template,css
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
 
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
-        reader = PdfReader(pdf)
-        for page in reader.pages:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
             text += page.extract_text()
     return text
 
 
 def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=512,
-        chunk_overlap=100,
-        length_function=len
-    )
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
     return chunks
 
-# Embeddings
-# For embedding we have two options:
-# 1. Use OpenAI AdaV2 model -> Currently ranked 6th on the leaderboard
-# 2. Use Instructor embedding model -> Currently ranked 2nd on the leaderboard
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
 
 
-def get_vectorStore(text_chunks):
-    # embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(
-        model_name="hkunlp/instructor-xl")
-    vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    print(vector_store)
-    return vector_store
+def get_conversational_chain():
 
-# Conversation chain
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details. Use the context as much as possible\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
 
+    Answer:
+    """
 
-def get_conversation_chain(vector_store):
-    # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":128})
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vector_store.as_retriever(),
-        memory=memory
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+
+    prompt = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
     )
-    return conversation_chain
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
-def handle_userInput(user_question):
-    chunks = get_text_chunks(user_question)
-    responses = []
-    for chunk in chunks:
-        response = st.session_state.conversation({'question': chunk})
-        responses.append(response)
-        st.session_state.chat_history.extend(response['chat_history'])
-    
-    for response in responses:
-        for i, message in enumerate(response['chat_history']):
-            if i % 2 == 0:
-                st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-            else:
-                st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+    return chain
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents": docs, "question": user_question}, return_only_outputs=True
+    )
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
 
 
 def main():
-    load_dotenv()
-    st.set_page_config(page_title="Sail", page_icon=":books:", layout="wide")
-    st.write(css, unsafe_allow_html=True)
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = ""
-        
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
 
-    st.header("Sail")
-    user_question = st.text_input("Ask a question about the doc")
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
     if user_question:
-        handle_userInput(user_question)
-    
+        user_input(user_question)
+
     with st.sidebar:
-        st.subheader("Your documents")
+        st.title("Menu:")
         pdf_docs = st.file_uploader(
-            "Upload a document", accept_multiple_files=True)
-        if st.button("Process"):
+            "Upload your PDF Files and Click on the Submit & Process Button",
+            accept_multiple_files=True,
+        )
+        if st.button("Submit & Process"):
             with st.spinner("Processing..."):
-
-                # Get Pdf text
                 raw_text = get_pdf_text(pdf_docs)
-
-                # Get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks)
-
-                # Create vector store
-                vector_store = get_vectorStore(text_chunks)
-
-                # Create conversation chain
-                # Making a variable session state does two things:
-                # 1. It stores the values across reruns of the script
-                # 2. It can be used outside the scope of the component where it is defined (sidebar in this case)
-                st.session_state.conversation = get_conversation_chain(
-                    vector_store)
+                get_vector_store(text_chunks)
+                st.success("Done")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
